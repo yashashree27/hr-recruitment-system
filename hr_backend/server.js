77427d2f33b23148a923fd18d0e3845244973ede
yashashree27 <yashashree37252@gmail.com>
2026-07-07 14:25:18 +0530

    updated server port for deployment

--- hr_backend/server.js
+++ hr_backend/server.js
@@ -6,8 +6,8 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
