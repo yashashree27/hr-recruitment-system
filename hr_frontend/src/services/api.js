6f9fa206e161695402b3970cfe34575a7f058638
Yashshree Marghade <yashashree37252@gmail.com>
2026-09-17 13:02:52 +0530

    Added kubernetes deployment configuration

--- hr_frontend/src/services/api.js
+++ hr_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hr-backend-0gyo.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
